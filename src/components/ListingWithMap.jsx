85935c9d6f88b47907bbcc3d16bce46ccff9233c
jessieh9 <104229166+jessieh9@users.noreply.github.com>
2024-01-27 13:44:32 -0800

    working on larger listings and maybe map

--- src/components/ListingWithMap.jsx
+++ src/components/ListingWithMap.jsx
@@ -1,10 +1,55 @@
 import React from "react";
-import { Grid, GridItem } from "@chakra-ui/react";
+import {
+  Grid,
+  GridItem,
+  Card,
+  Image,
+  Stack,
+  CardBody,
+  Heading,
+  Text,
+  CardFooter,
+  Button,
+} from "@chakra-ui/react";
 
 function ListingWithMap() {
   return (
     <Grid templateColumns="repeat(2, 1fr)" gap={2}>
-      <GridItem w="100%" h="10" bg="tomato" />
+      <GridItem w="100%" h="10">
+        <Card
+          direction={{ base: "column", sm: "row" }}
+          overflow="hidden"
+          variant="outline"
+        >
+          <Image
+            objectFit="cover"
+            maxW={{ base: "100%", sm: "200px" }}
+            src="https://images.unsplash.com/photo-1667489022797-ab608913feeb?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHw5fHx8ZW58MHx8fHw%3D&auto=format&fit=crop&w=800&q=60"
+            alt="Caffe Latte"
+          />
+
+          <Stack>
+            <CardBody>
+              <Heading size="md">Broadway</Heading>
+              <Text color={"gray"}>333 Silly Ave</Text>
+              <Text>$200 - $500</Text>
+              <Text>Apartment</Text>
+              <Text>Pet friendly, Non-smoking, Clean, Fitness Building</Text>
+              <Text py="2">
+                Caffè latte is a coffee beverage of Italian origin made with
+                espresso and steamed milk.
+              </Text>
+            </CardBody>
+
+            <CardFooter>
+              <Button variant="solid" colorScheme="blue">
+                Contact Lister
+              </Button>
+            </CardFooter>
+          </Stack>
+        </Card>
+      </GridItem>
+
       <GridItem w="100%" h="10" bg="papayawhip" />
     </Grid>
   );
