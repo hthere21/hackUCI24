314f8bc165924c4bb024283d697ca9d77b197e66
Richard Sima <richard.sima2021@gmail.com>
2024-01-26 23:52:13 -0800

    added profile page

---
+++ src/pages/Profile.jsx
@@ -0,0 +1,12 @@
+import * as React from "react";
+// import { Container, Box, Center, Text, Stack } from "@chakra-ui/react";
+
+function Profile() {
+  return (
+    <>
+      Testing
+    </>
+  );
+}
+
+export default Profile;
